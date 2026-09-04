4a99da70163fd28be4a9593dd952ddfb41f6db68
awjc <2312902+awjc@users.noreply.github.com>
2026-09-04 15:39:44 -0700

    trying other vite config path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Using './' makes all asset paths relative to index.html
-  // This is needed for GitHub Pages subpaths.
-  base: './',
+  base: '/CubeRoots/',
 });
