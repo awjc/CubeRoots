4ef1c267c686d1860b76bd9bbc3143dcb74935e0
awjc <2312902+awjc@users.noreply.github.com>
2026-09-07 13:32:25 -0700

    conditional narrowness for controls

--- src/ui/CubeControlPanel.ts
+++ src/ui/CubeControlPanel.ts
@@ -27,7 +27,7 @@ export class CubeControlPanel {
    * @param cube The Cube object to control.
    */
   constructor(private cubes: Cube[]) {
-    this.gui = new GUI({ width: 170 }); // make it a little narrower for mobile
+    this.gui = new GUI({ /* some options can go here */ });
     this.folders = new Map();
     this.deleteActionControllers = new Set();
   }
