73a661921f82c0fc7b1942de9d0c7e196b555910
awjc <2312902+awjc@users.noreply.github.com>
2026-09-09 12:19:51 -0700

    Demo emitting sphere light

--- src/components/Sphere.ts
+++ src/components/Sphere.ts
@@ -40,6 +40,10 @@ export class Sphere extends BaseObject {
     const material = initialSettings.flatShading ?
       new THREE.MeshPhongMaterial({
         color: initialSettings.color,
+        shininess: 100,
+        emissive: initialSettings.color,
+        // emissive: new THREE.Color('#ff00ff'),
+        emissiveIntensity: 0.2,
         flatShading: true
       }) :
       new THREE.MeshStandardMaterial({
@@ -47,9 +51,15 @@ export class Sphere extends BaseObject {
         metalness: initialSettings.metalness,
         roughness: initialSettings.roughness
       });
+    // Inside the Sphere constructor (src/components/Sphere.ts)
 
     const mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(initialSettings.position.x, initialSettings.position.y, initialSettings.position.z);
+
+
+    //  Emitting sphere
+    const light = new THREE.PointLight(initialSettings.color, 100); // Use the sphere's color
+    mesh.add(light);
 
     super(mesh);
     this.settings = initialSettings;
